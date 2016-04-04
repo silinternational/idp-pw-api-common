c14c2aa41eff84592853168c1d01db5b04a8782c
Phillip Shipley <phillip_shipley@sil.org>
2016-04-04 16:43:18 -0400

    rename AuthInterface to AuthnInterface

--- src/Auth/AuthnInterface.php
+++ src/Auth/AuthnInterface.php
@@ -1,18 +1,18 @@
 <?php
 namespace Sil\IdpPw\Common\Auth;
 
-interface AuthInterface
+interface AuthnInterface
 {
     /**
      * @param yii\web\Request|null $request
-     * @return User
+     * @return User|null If null is returned the user was redirected to IdP
      * @throws InvalidLoginException
      */
     public function login(yii\web\Request $request = null);
 
     /**
      * @param User|null $user
-     * @return boolean
+     * @return void
      */
     public function logout(User $user = null);
 }
