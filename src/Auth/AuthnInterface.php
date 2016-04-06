bc897864c602f8ff0c2754f15fb661a1075f954a
Phillip Shipley <phillip_shipley@sil.org>
2016-04-06 14:28:26 -0400

    added  param to auth interface

--- src/Auth/AuthnInterface.php
+++ src/Auth/AuthnInterface.php
@@ -7,14 +7,16 @@ interface AuthnInterface
 {
     /**
      * @param \yii\web\Request|null $request
+     * @param string $returnTo Where to have IdP send user after login
      * @return User|null If null is returned the user was redirected to IdP
      * @throws InvalidLoginException
      */
-    public function login(Request $request = null);
+    public function login(Request $request = null, $returnTo);
 
     /**
      * @param User|null $user
+     * @param string $returnTo Where to have IdP send user after login
      * @return void
      */
-    public function logout(User $user = null);
+    public function logout(User $user = null, $returnTo);
 }
