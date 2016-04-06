98d850e4f47870adf53aa8f895d2d743af6b412b
Phillip Shipley <phillip_shipley@sil.org>
2016-04-06 16:54:52 -0400

    updated phpdoc for auth login

--- src/Auth/AuthnInterface.php
+++ src/Auth/AuthnInterface.php
@@ -8,7 +8,7 @@ interface AuthnInterface
     /**
      * @param \yii\web\Request|null $request
      * @param string $returnTo Where to have IdP send user after login
-     * @return User|null If null is returned the user was redirected to IdP
+     * @return User|void
      * @throws InvalidLoginException
      */
     public function login(Request $request = null, $returnTo);
