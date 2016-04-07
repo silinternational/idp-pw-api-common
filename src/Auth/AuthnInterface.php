e0ca777c0de84664858a8a2eb8136d24a7f5bf34
Phillip Shipley <phillip_shipley@sil.org>
2016-04-07 08:33:07 -0400

    change parameter order for optional parameters last

--- src/Auth/AuthnInterface.php
+++ src/Auth/AuthnInterface.php
@@ -6,17 +6,17 @@ use yii\web\Request;
 interface AuthnInterface
 {
     /**
-     * @param \yii\web\Request|null $request
      * @param string $returnTo Where to have IdP send user after login
+     * @param \yii\web\Request|null $request
      * @return User|void
      * @throws InvalidLoginException
      */
-    public function login(Request $request = null, $returnTo);
+    public function login($returnTo, Request $request = null);
 
     /**
-     * @param User|null $user
      * @param string $returnTo Where to have IdP send user after login
+     * @param User|null $user
      * @return void
      */
-    public function logout(User $user = null, $returnTo);
+    public function logout($returnTo, User $user = null);
 }
