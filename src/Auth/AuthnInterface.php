2c382b7f7e1e9dd0b0304efbb9df08237ae18147
Phillip Shipley <phillip_shipley@sil.org>
2016-04-07 09:27:13 -0400

    added Auth\RedirectException for when user should be redirected to login/logout

--- src/Auth/AuthnInterface.php
+++ src/Auth/AuthnInterface.php
@@ -10,6 +10,7 @@ interface AuthnInterface
      * @param \yii\web\Request|null $request
      * @return User|void
      * @throws InvalidLoginException
+     * @throws RedirectException
      */
     public function login($returnTo, Request $request = null);
 
@@ -17,6 +18,7 @@ interface AuthnInterface
      * @param string $returnTo Where to have IdP send user after login
      * @param User|null $user
      * @return void
+     * @throws RedirectException
      */
     public function logout($returnTo, User $user = null);
 }
---
+++ src/Auth/RedirectException.php
@@ -0,0 +1,36 @@
+<?php
+namespace Sil\IdpPw\Common\Auth;
+
+/**
+ * Class RedirectException - Used to trigger a redirect to perform login/logout
+ * when IdP uses this method, such as with SAML
+ * @package Sil\IdpPw\Common\Auth
+ */
+class RedirectException extends \Exception
+{
+    /**
+     * @var string
+     */
+    public $url;
+
+    /**
+     * RedirectException constructor.
+     * @param string $url
+     * @param string $message
+     * @param int $code
+     * @param \Exception $previous
+     */
+    public function __construct($url, $message, $code, \Exception $previous)
+    {
+        $this->url = $url;
+        parent::__construct($message, $code, $previous);
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+}
